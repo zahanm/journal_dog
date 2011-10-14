e6bf310101caaa7818bce15c5d88dccf6c28926c
Zahan Malkani <zahanm@gmail.com>
2011-10-14 00:07:02 -0700

    Adding in python for pdfsplit

---
+++ pdfsplit.py
@@ -0,0 +1,23 @@
+
+from __future__ import print_function
+
+import sys
+from pyPdf import PdfFileWriter, PdfFileReader
+
+def split_pages(pdf_fname):
+  out_fnames = []
+  with open(pdf_fname, 'rb') as inp_file:
+    inp = PdfFileReader(inp_file)
+    for page in xrange(inp.getNumPages()):
+      out = PdfFileWriter()
+      out.addPage(inp.getPage(page))
+      out_fnames.append('out_{0}.pdf'.format(page))
+      with open(out_fnames[page], 'wb') as out_file:
+        out.write(out_file)
+  return out_fnames
+
+if __name__ == '__main__':
+  if(len(sys.argv) == 2):
+    split_pages(sys.argv[1])
+  else:
+    print('usage: python', __file__, '<input_pdf>')
